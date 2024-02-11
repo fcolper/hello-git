2c3b0dd40567fd521af21622b06d3fcaa17df8cc
FabianColper <fabian.colper@gmail.com>
2024-02-11 12:12:16 -0300

    Hello Github

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,10 @@
 print('New Hello Git')
+# git init para iniciar
+# git branch -m main #para revisar rama principal
+# git status para ver el estado
+# git commit 
+# git checkout borra una modificación
+# git reset reinicia casi completo
+# git log muestra los detalles
+# git tree para ver alias y los commit de manera rápida
+#https://training.github.com/downloads/es_ES/github-git-cheat-sheet.pdf
--- hellogit3.py
+++ hellogit3.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-print('helo git 3 v login')
-=======
 print('helo git 3 v3')
->>>>>>> main
+print("hello GITHUB! Welcome")
+
