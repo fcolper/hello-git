600754ca6cd9ae1cb1d5d91a96b552a3c86eabe2
FCOLPER <151382524+fcolper@users.noreply.github.com>
2024-02-13 22:00:24 -0300

    Update hellogit.py
    #Agregando nuevos pasos para github

--- hellogit.py
+++ hellogit.py
@@ -1,4 +1,4 @@
-print('New Hello Git')
+print('New Hello GitHub Desktop')
 # git init para iniciar
 # git branch -m main #para revisar rama principal
 # git status para ver el estado
@@ -8,3 +8,14 @@ print('New Hello Git')
 # git log muestra los detalles
 # git tree para ver alias y los commit de manera rápida
 #https://training.github.com/downloads/es_ES/github-git-cheat-sheet.pdf
+#Para GITHUB:
+#Revisar si hay que hacer git commit**
+#Ir a .ssh con la cmd
+#C:\Users\fabia>ssh-add c:/Users/fabia/.ssh/id_rsa
+#C:\Users\fabia\.ssh>ssh -T git@github.com
+#Ir a la carpeta local del repo y copiar la clave SSH
+#C:\Users\fabia\Escritorio\Hola_NHLData>git remote add origin git@github.com:fcolper/NHL_stats_teams_2023-2024.git
+#C:\Users\fabia\Escritorio\Hola_NHLData>git push -u origin main
+#C:\Users\fabia\Escritorio\Hola_NHLData>git fetch
+#C:\Users\fabia\Escritorio\Hola_NHLData>git pull origin main --allow-unrelated-histories
+#C:\Users\fabia\Escritorio\Hola_NHLData>git push origin main
